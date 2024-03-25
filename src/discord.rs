866dd73384ef344a9cf4bdeffca612efdef1f352
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-03-25 23:43:13 +0100

    add a simple calendar example

--- src/discord.rs
+++ src/discord.rs
@@ -1,8 +1,10 @@
+use crate::calendar::UpdateCalendarEvent;
 use crate::commands;
 use crate::types;
 use anyhow::Error;
 use log::{debug, info};
 use poise::serenity_prelude as serenity;
+use serenity::http::CacheHttp;
 
 async fn on_error(error: poise::FrameworkError<'_, types::Data, Error>) {
     // This is our custom error handler
@@ -20,11 +22,11 @@ async fn on_error(error: poise::FrameworkError<'_, types::Data, Error>) {
         }
     }
 }
-
 pub struct Discord {
     token: String,
     data: types::Data,
     intents: serenity::GatewayIntents,
+    rcv: tokio::sync::mpsc::Receiver<UpdateCalendarEvent>,
 }
 
 impl Discord {
@@ -32,11 +34,13 @@ impl Discord {
         token: String,
         data: types::Data,
         gateway_intents: serenity::GatewayIntents,
+        rcv: tokio::sync::mpsc::Receiver<UpdateCalendarEvent>,
     ) -> Self {
         Self {
             token,
             data,
             intents: gateway_intents,
+            rcv,
         }
     }
 
@@ -51,19 +55,19 @@ impl Discord {
                 on_error: |error| Box::pin(async move { on_error(error).await }),
                 pre_command: |ctx| {
                     Box::pin(async move {
-                        let channel_name = &ctx
-                            .channel_id()
-                            .name(&ctx)
-                            .await
-                            .unwrap_or_else(|_| "<unknown>".to_owned());
-                        let author = &ctx.author().name;
+                        // let channel_name = &ctx
+                        //     .channel_id()
+                        //     .name(&ctx)
+                        //     .await
+                        //     .unwrap_or_else(|_| "<unknown>".to_owned());
+                        // let author = &ctx.author().name;
 
-                        info!(
-                            "{} in {} used slash command '{}'",
-                            author,
-                            channel_name,
-                            &ctx.invoked_command_name()
-                        );
+                        // info!(
+                        //     "{} in {} used slash command '{}'",
+                        //     author,
+                        //     channel_name,
+                        //     &ctx.invoked_command_name()
+                        // );
                     })
                 },
                 post_command: |ctx| {
@@ -78,6 +82,24 @@ impl Discord {
                 ..Default::default()
             })
             .setup(move |ctx, ready, framework| {
+                let ctx2 = ctx.clone();
+                tokio::spawn(async move {
+                    let mut rx = self.rcv;
+                    while let Some(event) = rx.recv().await {
+                        debug!("Received event for calendar {}", event.calendar_id);
+                        let channel = serenity::ChannelId::new(1102198299093647470);
+
+                        let message = serenity::CreateMessage::new().content(format!(
+                            "New {} events {}",
+                            event.calendar_id,
+                            event.new_events.len()
+                        ));
+
+                        let res = channel.send_message(ctx2.http(), message).await;
+                        println!("{:?}", res);
+                    }
+                });
+
                 Box::pin(async move {
                     debug!("Registering commands..");
                     poise::builtins::register_globally(ctx, &framework.options().commands).await?;
--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,22 @@ Calendarbot  Copyright (C) 2023 Zbinden Yohan
 This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
 This is free software, and you are welcome to redistribute it
  */
+mod calendar;
 mod commands;
-pub mod discord;
+mod discord;
 pub mod models;
 pub mod schema;
 pub mod types;
 
 use anyhow::Error;
+use calendar::UpdateCalendarEvent;
 use diesel::pg::PgConnection;
-use diesel::prelude::*;
+use diesel::r2d2::{ConnectionManager, Pool};
 use diesel_migrations::{embed_migrations, EmbeddedMigrations};
+use log::info;
 use poise::serenity_prelude as serenity;
-use std::env;
+use std::time::Duration;
+use std::{env, thread};
 
 use dotenvy::dotenv;
 
@@ -23,21 +27,42 @@ type Context<'a> = poise::Context<'a, types::Data, Error>;
 
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations/");
 
+pub fn get_connection_pool(database_url: String) -> Pool<ConnectionManager<PgConnection>> {
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    Pool::builder()
+        .test_on_check_out(true)
+        .build(manager)
+        .expect("Failed to create pool.")
+}
+
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     env_logger::init();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let connection = PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+    let pool = get_connection_pool(database_url);
 
     let token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN not found");
     let intents = serenity::GatewayIntents::non_privileged();
 
-    let data = types::Data::new(connection).expect("Unable to load config!");
+    let data = types::Data::new(pool.clone()).expect("Unable to load config!");
 
-    let mut client = discord::Discord::new(token, data, intents).init().await;
+    let (tx, rx) = tokio::sync::mpsc::channel::<UpdateCalendarEvent>(200);
+
+    tokio::spawn(async move {
+        let g_client = calendar::GCalendar::new(pool.clone())
+            .await
+            .expect("Unable to connect to google calendar");
+
+        loop {
+            info!("Updating calendars");
+            g_client.update_calendars(tx.clone()).await;
+            thread::sleep(Duration::from_secs(5));
+        }
+    });
+
+    let mut client = discord::Discord::new(token, data, intents, rx).init().await;
 
     if let Err(why) = client.start().await {
         println!("An error occured while running the client: {:?}", why);
