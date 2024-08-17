0af97a3a28d0144f3234d952495acd46ecf99401
Yohan Zbinden <46626633+yoyozbi@users.noreply.github.com>
2024-08-17 14:04:58 +0200

    Google Calendar (#10)
    * add a simple calendar example
    
    * correctly implement channels and don't send message when events didn't change
    
    * remove worker_thread and edit message when new data arrives
    
    * add better date formatting
    
    * change date format
    
    * update schema and update flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,15 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
     fenix = {
-        url = "github:nix-community/fenix";
-        inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-formatter-pack = {
+      url = "github:Gerschtli/nix-formatter-pack";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
@@ -14,48 +18,67 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+  outputs =
+    { self
+    , nixpkgs
+    , devenv
+    , systems
+    , nix-formatter-pack
+    , ...
+    } @ inputs:
     let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
       packages = forEachSystem (system: {
         devenv-up = self.devShells.${system}.default.config.procfileScript;
       });
-
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  pre-commit.hooks = {
-										cargo-check.enable = true;
-										rustfmt.enable = true;
-										docker = {
-											enable = true;
-											name ="Docker linting";
-											entry = "ghcr.io/hadolint/hadolint hadolint ./Dockerfile";
-											files="Dockerfile";
-											language="docker_image";
-											pass_filenames = false;
-										};
-				  				};
-                  # https://devenv.sh/reference/options/
-                  packages = [ pkgs.openssl.dev pkgs.pkgconfig pkgs.dbus.dev pkgs.postgresql pkgs.hadolint ];
-                  languages.rust = {
-                    enable = true;
-                    channel = "stable";
-										components = ["rustfmt" "clippy"];
-                  };
-                  env.OPENSSL_DEV=pkgs.openssl.dev;
-                }
-              ];
-            };
-          });
+      formatter = nixpkgs.lib.genAttrs supportedSystems (system:
+        nix-formatter-pack.lib.mkFormatter {
+          pkgs = nixpkgs.legacyPackages.${system};
+          config.tools = {
+            alejandra.enable = true;
+            deadnix.enable = true;
+            nixpkgs-fmt.enable = true;
+            statix.enable = true;
+          };
+        });
+      devShells =
+        forEachSystem
+          (system:
+            let
+              pkgs = nixpkgs.legacyPackages.${system};
+            in
+            {
+              default = devenv.lib.mkShell {
+                inherit inputs pkgs;
+                modules = [
+                  {
+                    pre-commit.hooks = {
+                      cargo-check.enable = true;
+                      rustfmt.enable = true;
+                      docker = {
+                        enable = true;
+                        name = "Docker linting";
+                        entry = "ghcr.io/hadolint/hadolint hadolint ./Dockerfile";
+                        files = "Dockerfile";
+                        language = "docker_image";
+                        pass_filenames = false;
+                      };
+                    };
+                    # https://devenv.sh/reference/options/
+                    packages = [ pkgs.openssl.dev pkgs.pkg-config pkgs.dbus.dev pkgs.postgresql pkgs.hadolint ];
+                    dotenv.disableHint = true;
+                    languages.rust = {
+                      enable = true;
+                      channel = "stable";
+                      components = [ "rustfmt" "clippy" ];
+                    };
+                    env.OPENSSL_DEV = pkgs.openssl.dev;
+                  }
+                ];
+              };
+            });
     };
 }
--- src/discord.rs
+++ src/discord.rs
@@ -1,8 +1,17 @@
+use crate::calendar::GCalendar;
+use crate::calendar::UpdateCalendarEvent;
 use crate::commands;
 use crate::types;
-use anyhow::Error;
-use log::{debug, info};
+use anyhow::{Error, Result};
+use google_calendar3::api::Event;
+use google_calendar3::chrono::{Datelike, NaiveDate, Utc};
+use log::{debug, error, info};
 use poise::serenity_prelude as serenity;
+use std::collections::btree_map::Entry;
+use std::collections::BTreeMap;
+use std::sync::Arc;
+use tokio::sync::mpsc;
+use tokio::sync::Mutex;
 
 async fn on_error(error: poise::FrameworkError<'_, types::Data, Error>) {
     // This is our custom error handler
@@ -21,26 +30,52 @@ async fn on_error(error: poise::FrameworkError<'_, types::Data, Error>) {
     }
 }
 
+#[derive(Clone)]
+struct LocalCache {
+    cache: Arc<serenity::Cache>,
+    client: Arc<serenity::Http>,
+}
+impl LocalCache {
+    fn new(client: Arc<serenity::Http>) -> Self {
+        Self {
+            cache: Arc::new(serenity::Cache::default()),
+            client,
+        }
+    }
+}
+
+impl serenity::CacheHttp for LocalCache {
+    fn http(&self) -> &serenity::Http {
+        &self.client
+    }
+
+    fn cache(&self) -> Option<&Arc<serenity::Cache>> {
+        Some(&self.cache)
+    }
+}
+
 pub struct Discord {
     token: String,
-    data: types::Data,
     intents: serenity::GatewayIntents,
+    cache: Arc<Mutex<Option<LocalCache>>>,
 }
 
 impl Discord {
-    pub fn new(
-        token: String,
-        data: types::Data,
-        gateway_intents: serenity::GatewayIntents,
-    ) -> Self {
+    pub fn new(token: String, gateway_intents: serenity::GatewayIntents) -> Self {
         Self {
             token,
-            data,
             intents: gateway_intents,
+            cache: Arc::new(Mutex::new(None)),
         }
     }
 
-    pub async fn init(self) -> serenity::Client {
+    pub async fn init(
+        &mut self,
+        calendar_rx: mpsc::Receiver<UpdateCalendarEvent>,
+        data: types::Data,
+    ) -> serenity::Client {
+        let cache_clone = self.cache.clone();
+
         let framework = poise::Framework::builder()
             .options(poise::FrameworkOptions {
                 commands: vec![
@@ -79,6 +114,13 @@ impl Discord {
             })
             .setup(move |ctx, ready, framework| {
                 Box::pin(async move {
+                    cache_clone
+                        .lock()
+                        .await
+                        .replace(LocalCache::new(ctx.http.clone()));
+
+                    Discord::new_data_thread(calendar_rx, cache_clone.clone());
+
                     debug!("Registering commands..");
                     poise::builtins::register_globally(ctx, &framework.options().commands).await?;
 
@@ -87,14 +129,162 @@ impl Discord {
 
                     info!("{} is ready !", ready.user.name);
 
-                    Ok(self.data)
+                    Ok(data)
                 })
             })
             .build();
 
-        serenity::Client::builder(self.token, self.intents)
+        let it = self.intents.clone();
+        let client = serenity::Client::builder(self.token.clone(), it)
             .framework(framework)
             .await
-            .expect("Failed to create client")
+            .expect("Failed to create client");
+
+        return client;
+    }
+
+    fn new_data_thread(
+        mut calendar_rx: mpsc::Receiver<UpdateCalendarEvent>,
+        cache: Arc<Mutex<Option<LocalCache>>>,
+    ) {
+        tokio::spawn(async move {
+            let mut events_cache: BTreeMap<String, Vec<Event>> = BTreeMap::new();
+            let mut message_cache: BTreeMap<String, u64> = BTreeMap::new();
+            let channel = serenity::ChannelId::new(1102198299093647470);
+
+            while let Some(event) = calendar_rx.recv().await {
+                debug!("Received event for calendar {}", event.calendar_id);
+
+                let events = events_cache
+                    .entry(event.calendar_id.clone())
+                    .or_insert(Vec::new());
+                let matching = events
+                    .iter()
+                    .zip(event.new_events.iter())
+                    .filter(|&(a, b)| GCalendar::compare_event(a, b))
+                    .count();
+
+                let do_match = matching == event.new_events.len() && matching == events.len();
+                debug!(
+                    "matching: {} == {} && {} == {}",
+                    matching,
+                    event.new_events.len(),
+                    matching,
+                    events.len()
+                );
+
+                if do_match && !events.is_empty() {
+                    debug!("No new events");
+                    continue;
+                }
+
+                let cache = cache.as_ref().lock().await.clone().unwrap();
+                let embed = match Discord::event_to_embed(event.new_events.clone()) {
+                    Ok(v) => v,
+                    Err(_) => serenity::CreateEmbed::new().title("Events").field(
+                        "Error",
+                        "Error getting events",
+                        true,
+                    ),
+                };
+
+                let mut create_new_message = false;
+                if let Entry::Occupied(o) = message_cache.entry(event.calendar_id.clone()) {
+                    let message_id = serenity::MessageId::new(o.get().clone());
+
+                    let err = cache
+                        .client
+                        .edit_message(
+                            channel,
+                            message_id,
+                            &serenity::EditMessage::new().add_embed(embed.clone()),
+                            Vec::new(),
+                        )
+                        .await;
+
+                    match err {
+                        Err(e) => {
+                            error!("Failed to edit message ({}): {}", message_id.clone(), e);
+                            create_new_message = true;
+                        }
+                        _ => (),
+                    };
+                } else {
+                    create_new_message = true;
+                }
+
+                if create_new_message {
+                    let res = channel
+                        .send_message(cache, serenity::CreateMessage::new().add_embed(embed))
+                        .await
+                        .unwrap();
+
+                    message_cache
+                        .entry(event.calendar_id.clone())
+                        .or_insert(res.id.get());
+                }
+
+                if !events.is_empty() {
+                    events.clear();
+                }
+                events.extend(event.new_events.clone());
+            }
+        });
+    }
+
+    fn event_to_embed(events: Vec<Event>) -> Result<serenity::CreateEmbed> {
+        let mut sorted: BTreeMap<(NaiveDate, NaiveDate), Vec<Event>> = BTreeMap::new();
+        let mut fields: Vec<(String, String, bool)> = vec![];
+        for ele in events {
+            let ele_clone = ele.clone();
+            let start_date = ele_clone.start.unwrap().date_time.unwrap();
+            let end_date = ele.clone().end.unwrap().date_time.unwrap();
+            sorted
+                .entry((start_date.date_naive(), end_date.date_naive()))
+                .or_insert(Vec::new())
+                .push(ele);
+        }
+
+        for ((start_date, end_date), events) in sorted.iter() {
+            let mut field = String::new();
+            for event in events {
+                field.push_str(&format!(
+                    "```{} - {} | {}```\n",
+                    event
+                        .start
+                        .clone()
+                        .unwrap()
+                        .date_time
+                        .unwrap()
+                        .format("%H:%M"),
+                    event
+                        .end
+                        .clone()
+                        .unwrap()
+                        .date_time
+                        .unwrap()
+                        .format("%H:%M"),
+                    event.summary.clone().unwrap()
+                ));
+            }
+            let mut format = String::from("**%A** - %e %B");
+            if start_date.year() != end_date.year() || start_date.year() != Utc::now().year() {
+                format = String::from("%F");
+            }
+
+            let mut key = start_date.format(&format.clone()).to_string();
+
+            if start_date.format("%F").to_string() != end_date.format("%F").to_string() {
+                key.push_str(
+                    &end_date
+                        .format(format!(" // {}", format.clone()).as_str())
+                        .to_string(),
+                );
+            }
+
+            fields.push((key, field, false));
+        }
+
+        Ok(serenity::CreateEmbed::new().title("Events").fields(fields))
     }
 }
